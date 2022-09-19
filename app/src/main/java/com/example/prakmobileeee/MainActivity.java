0c4700646e2a62fc2a95405853ae341ec54fb1af
ilhamfariza <ilham.fariza@mhs.unsoed.ac.id>
2022-09-19 14:15:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prakmobileeee/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prakmobileeee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
